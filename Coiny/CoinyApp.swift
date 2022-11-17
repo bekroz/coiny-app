fbb59e078673aa8a2aa13663538f59d3f4cf6747
BEK ROZ <bekrozikoff@gmail.com>
2022-11-17 23:35:48 +0500

    Initial Commit

---
+++ Coiny/CoinyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinyApp.swift
+//  Coiny
+//
+//  Created by BekRoz on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CoinyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
