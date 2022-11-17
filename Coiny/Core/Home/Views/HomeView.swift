524111d2462461b27ef3ba1e2c6ad29326629da2
BEK ROZ <bekrozikoff@gmail.com>
2022-11-17 23:52:31 +0500

    update content view

---
+++ Coiny/Core/Home/Views/HomeView.swift
@@ -0,0 +1,28 @@
+//
+//  HomeView.swift
+//  Coiny
+//
+//  Created by BekRoz on 17/11/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                
+                // MARK: Top Movers View
+                
+                // MARK: All Coins View
+            }
+            .navigationTitle("Live Prices")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
